c64b062eaff7be3c1a77307ba228fcecc57d652d
mutasim chaudhary <mutasim.chaud@gmail.com>
2024-03-28 23:51:52 +0000

    portfolio

---
+++ src/assets/data/github.js
@@ -0,0 +1,40 @@
+import html from '../images/html.png';
+import advanced from '../images/advancedCSS.PNG';
+import OOP from '../images/OOP.PNG';
+import pwa from '../images/PWA.PNG';
+import api from './images/api.gif';
+
+const github = [
+    {
+        name : "01-HTML-CSS-and-Git-Code-Refactor-Mutasim",
+        image: html,
+        github: 'https://github.com/Mutasim-c/01-HTML-CSS-and-Git-Code-Refactor-Mutasim'
+
+    },
+    {
+        name : "Advanced-CSS-Portfolio-Mutasim",
+        image: advanced,
+        github: 'https://github.com/Mutasim-c/Advanced-CSS-Portfolio-Mutasim' 
+    }
+    ,
+    {
+        name : "OOP-SVG-logo-Maker-Mutasim",
+        image: OOP,
+        github: 'https://github.com/Mutasim-c/OOP-SVG-logo-Maker-Mutasim' 
+    }
+    ,
+    {
+        name : "Server-Side-APIs-Weather-Dashboard",
+        image: api,
+        github: 'https://github.com/Mutasim-c/Server-Side-APIs-Weather-Dashboard' 
+    }
+    ,
+    {
+        name : "PWA-Text-Editor-Mutasim",
+        image: pwa,
+        github: 'https://github.com/Mutasim-c/PWA-Text-Editor-Mutasim' 
+    },
+
+];
+
+export default github;
